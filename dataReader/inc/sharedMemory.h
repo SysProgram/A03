796106e91a6cd11f06413649b021fb63ab36e356
YC-Cho <yccho8212@gmail.com>
2020-03-18 17:57:33 -0700

    master

---
+++ dataReader/inc/sharedMemory.h
@@ -0,0 +1,29 @@
+/*!
+ *
+ *	@file	:	sharedMemory.h
+ *
+ *	project	:	PROG1970 - Systems Programming - The Hoochamacallit System
+ *
+ *	@author	:	 *
+ *	@date	:	2018-07-17
+ *
+ *	@brief	:	This fle contains data an function prototypes used for the function defined in
+ *				../src/sharedMemory.c.
+ *
+ */
+
+
+#ifndef SHAREDMEMORY_H
+#define SHAREDMEMORY_H
+
+
+#define SHMGET_ERROR -1
+#define SHARED_MEMORY_DOES_NOT_EXIST -1
+#define FTOK_ERROR -1
+#define SHARED_MEMORY_ERROR -1
+
+
+int getSharedMemoryID();
+
+
+#endif /* SHAREDMEMORY_H */
